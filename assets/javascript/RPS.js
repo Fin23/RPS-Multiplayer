eeb9487eaae0f688832118e61f75056ac60272d4
Ross Jameson <Rossjameson1623@gmail.com>
2019-07-20 08:18:46 -0500

    90% done with css begining main javascript declaring variables

---
+++ assets/javascript/RPS.js
@@ -0,0 +1,9 @@
+const userScore = 0;
+const computerScore =0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_button = document.getElementById("Rock");
+const paper_button = document.getElementById("Paper");
+const scissors_button = document.getElementById("Scissors");
